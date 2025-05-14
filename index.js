b1ea335cf23568320ac929c99d34ebfecb6f2b76
Samuel Kim <samjkim455@gmail.com>
2025-05-14 13:56:20 -0400

    final console submission

---
+++ index.js
@@ -0,0 +1,7 @@
+const code1 = 7 + 3;
+const code2 = 100 - 120 / 2;
+const code3 = 79 % 40;
+const message = "The vault has been secured. The combination is: "
+const codeA = code1 + `-` + code2 + `-` + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
